5c982374fa94fe1048df627a59642eb5503bdc55
bayashi <bayashi@cpan.org>
2023-04-14 12:29:21 +0000

    Add render tests

--- render_test.go
+++ render_test.go
@@ -18,19 +18,73 @@ func TestRoot(t *testing.T) {
 	expect := `
 ┌ root
 `
-
 	tree := render(Node("root"))
-
 	actually.Got(tree).Expect(expect).Same(t)
 }
 
-func TestRoot1Child(t *testing.T) {
+func TestRootChild(t *testing.T) {
 	expect := `
 ┌ root
 └── Child
 `
-
 	tree := render(Node("root").Append(Node("Child")))
+	actually.Got(tree).Expect(expect).Same(t)
+}
 
+func TestRootChildGChild(t *testing.T) {
+	expect := `
+┌ root
+└─┬ Child
+  └── G-Child
+`
+	tree := render(Node("root").Append(Node("Child").Append(Node("G-Child"))))
+	actually.Got(tree).Expect(expect).Same(t)
+}
+
+func TestRootChildWithProp(t *testing.T) {
+	expect := `
+┌* root: tag
+│   root description
+└──+ Child: tag
+      Child description
+`
+	root  := Node("root").Icon("*").Tag("tag").Description("root description")
+	child := Node("Child").Icon("+").Tag("tag").Description("Child description")
+	tree := render(root.Append(child))
+	actually.Got(tree).Expect(expect).Same(t)
+}
+
+func TestRootChildGChildWithProp(t *testing.T) {
+	expect := `
+┌* root: tag
+│   root description
+└─┬+ Child: tag
+  │   Child description
+  └──$ G-Child: tag
+        G-Child description
+`
+	root   := Node("root").Icon("*").Tag("tag").Description("root description")
+	child  := Node("Child").Icon("+").Tag("tag").Description("Child description")
+	gchild := Node("G-Child").Icon("$").Tag("tag").Description("G-Child description")
+	tree := render(root.Append(child.Append(gchild)))
+	actually.Got(tree).Expect(expect).Same(t)
+}
+
+func TestRootChildrenGChildWithProp(t *testing.T) {
+	expect := `
+┌* root: tag
+│   root description
+├─┬+ Child: tag
+│ │   Child description
+│ └──$ G-Child: tag
+│       G-Child description
+└──+ Child2: tag
+      Child2 description
+`
+	root   := Node("root").Icon("*").Tag("tag").Description("root description")
+	child  := Node("Child").Icon("+").Tag("tag").Description("Child description")
+	gchild := Node("G-Child").Icon("$").Tag("tag").Description("G-Child description")
+	child2 := Node("Child2").Icon("+").Tag("tag").Description("Child2 description")
+	tree := render(root.Append(child.Append(gchild)).Append(child2))
 	actually.Got(tree).Expect(expect).Same(t)
 }
