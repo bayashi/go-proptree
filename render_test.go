c8b4b24ddd5d9a3660db479b01eff79e060151fc
bayashi <bayashi@cpan.org>
2023-04-18 14:45:26 +0000

    Add render test for relax mode

--- render_test.go
+++ render_test.go
@@ -38,7 +38,7 @@ func TestRootChildGChild(t *testing.T) {
   └── G-Child
 `
 	tree := render(Node("root").Append(Node("Child").Append(Node("G-Child"))))
-	actually.Got(tree).Expect(expect).Same(t)
+	actually.Got(tree).Expect(expect).ShowRawData().Same(t)
 }
 
 func TestRootChildWithProp(t *testing.T) {
@@ -51,7 +51,7 @@ func TestRootChildWithProp(t *testing.T) {
 	root  := Node("root").Icon("*").Tag("tag").Description("root description")
 	child := Node("Child").Icon("+").Tag("tag").Description("Child description")
 	tree := render(root.Append(child))
-	actually.Got(tree).Expect(expect).Same(t)
+	actually.Got(tree).Expect(expect).ShowRawData().Same(t)
 }
 
 func TestRootChildGChildWithProp(t *testing.T) {
@@ -67,7 +67,7 @@ func TestRootChildGChildWithProp(t *testing.T) {
 	child  := Node("Child").Icon("+").Tag("tag").Description("Child description")
 	gchild := Node("G-Child").Icon("$").Tag("tag").Description("G-Child description")
 	tree := root.Append(child.Append(gchild))
-	actually.Got(render(tree)).Expect(expect).Same(t)
+	actually.Got(render(tree)).Expect(expect).ShowRawData().Same(t)
 }
 
 func TestRoot2ChildrenGChildWithProp(t *testing.T) {
@@ -86,7 +86,7 @@ func TestRoot2ChildrenGChildWithProp(t *testing.T) {
 	gchild := Node("G-Child").Icon("$").Tag("tag").Description("G-Child description")
 	child2 := Node("Child2").Icon("+").Tag("tag").Description("Child2 description")
 	tree := root.Append(child.Append(gchild)).Append(child2)
-	actually.Got(render(tree)).Expect(expect).Same(t)
+	actually.Got(render(tree)).Expect(expect).ShowRawData().Same(t)
 }
 
 func TestRoot3ChildrenGChildWithProp(t *testing.T) {
@@ -108,5 +108,132 @@ func TestRoot3ChildrenGChildWithProp(t *testing.T) {
 	child2 := Node("Child2").Icon("+").Tag("tag").Description("Child2 description")
 	child3 := Node("Child3").Icon("+").Tag("tag").Description("Child3 description")
 	tree := root.Append(child.Append(gchild)).Append(child2).Append(child3)
-	actually.Got(render(tree)).Expect(expect).Same(t)
+	actually.Got(render(tree)).Expect(expect).ShowRawData().Same(t)
+}
+
+//----- relax mode
+
+func renderWithO(n *N, o *RenderTextOptions) string {
+	buf := &bytes.Buffer{}
+	n.RenderAsText(buf, o)
+
+	return buf.String()
+}
+
+var o = RenderTextDefaultOptions().Relax()
+
+func TestRelaxRoot(t *testing.T) {
+	expect := `
+ ┌ root
+ 
+`
+	tree := renderWithO(Node("root"), o)
+	actually.Got(tree).Expect(expect).Same(t)
+}
+
+func TestRelaxRootChild(t *testing.T) {
+	expect := `
+ ┌ root
+ │
+ └──── Child
+ 
+`
+	tree := renderWithO(Node("root").Append(Node("Child")), o)
+	actually.Got(tree).Expect(expect).ShowRawData().Same(t)
+}
+
+func TestRelaxRootChildGChild(t *testing.T) {
+	expect := `
+ ┌ root
+ │
+ └───┬ Child
+     │
+     └──── G-Child
+     
+`
+	tree := renderWithO(Node("root").Append(Node("Child").Append(Node("G-Child"))), o)
+	actually.Got(tree).Expect(expect).ShowRawData().Same(t)
+}
+
+func TestRelaxRootChildWithProp(t *testing.T) {
+	expect := `
+ ┌* root: tag
+ │   root description
+ │
+ └────+ Child: tag
+         Child description
+ 
+`
+	root  := Node("root").Icon("*").Tag("tag").Description("root description")
+	child := Node("Child").Icon("+").Tag("tag").Description("Child description")
+	tree := renderWithO(root.Append(child), o)
+	actually.Got(tree).Expect(expect).ShowRawData().Same(t)
+}
+
+func TestRelaxRootChildGChildWithProp(t *testing.T) {
+	expect := `
+ ┌* root: tag
+ │   root description
+ │
+ └───┬+ Child: tag
+     │   Child description
+     │
+     └────$ G-Child: tag
+             G-Child description
+     
+`
+	root   := Node("root").Icon("*").Tag("tag").Description("root description")
+	child  := Node("Child").Icon("+").Tag("tag").Description("Child description")
+	gchild := Node("G-Child").Icon("$").Tag("tag").Description("G-Child description")
+	tree := root.Append(child.Append(gchild))
+	actually.Got(renderWithO(tree, o)).Expect(expect).ShowRawData().Same(t)
+}
+
+func TestRelaxRoot2ChildrenGChildWithProp(t *testing.T) {
+	expect := `
+ ┌* root: tag
+ │   root description
+ │
+ ├───┬+ Child: tag
+ │   │   Child description
+ │   │
+ │   └────$ G-Child: tag
+ │           G-Child description
+ │   
+ └────+ Child2: tag
+         Child2 description
+ 
+`
+	root   := Node("root").Icon("*").Tag("tag").Description("root description")
+	child  := Node("Child").Icon("+").Tag("tag").Description("Child description")
+	gchild := Node("G-Child").Icon("$").Tag("tag").Description("G-Child description")
+	child2 := Node("Child2").Icon("+").Tag("tag").Description("Child2 description")
+	tree := root.Append(child.Append(gchild)).Append(child2)
+	actually.Got(renderWithO(tree, o)).Expect(expect).ShowRawData().Same(t)
+}
+
+func TestRelaxRoot3ChildrenGChildWithProp(t *testing.T) {
+	expect := `
+ ┌* root: tag
+ │   root description
+ │
+ ├───┬+ Child: tag
+ │   │   Child description
+ │   │
+ │   └────$ G-Child: tag
+ │           G-Child description
+ │   
+ ├────+ Child2: tag
+ │       Child2 description
+ └────+ Child3: tag
+         Child3 description
+ 
+`
+	root   := Node("root").Icon("*").Tag("tag").Description("root description")
+	child  := Node("Child").Icon("+").Tag("tag").Description("Child description")
+	gchild := Node("G-Child").Icon("$").Tag("tag").Description("G-Child description")
+	child2 := Node("Child2").Icon("+").Tag("tag").Description("Child2 description")
+	child3 := Node("Child3").Icon("+").Tag("tag").Description("Child3 description")
+	tree := root.Append(child.Append(gchild)).Append(child2).Append(child3)
+	actually.Got(renderWithO(tree, o)).Expect(expect).ShowRawData().Same(t)
 }
